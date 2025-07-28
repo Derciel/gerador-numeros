aeb4f5c1bf18375f818b92625d3b6bc71a31364d
Derciel Alves dos Santos Junior <89181800+Derciel@users.noreply.github.com>
2025-07-28 18:12:42 -0300

    Update config.py
    remoção de vunerabilidade

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()  # Carrega variáveis do .env
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://chamados-db_owner:npg_9VCOSDiX3yJP@ep-restless-grass-ac9yjj45-pooler.sa-east-1.aws.neon.tech/chamados-db?sslmode=require&channel_binding=require'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
